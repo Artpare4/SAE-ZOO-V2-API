cb57f7e979a6e7a57a2f5e6c55114775eaa62dd0
pare0028 <arthur.parent@etudidant.univ-reims.fr>
2024-02-01 18:32:16 +0100

    Ajout de la ressource Api ZoneParc

--- src/Entity/ZoneParc.php
+++ src/Entity/ZoneParc.php
@@ -2,12 +2,14 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiResource;
 use App\Repository\ZoneParcRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: ZoneParcRepository::class)]
+#[ApiResource]
 class ZoneParc
 {
     #[ORM\Id]
