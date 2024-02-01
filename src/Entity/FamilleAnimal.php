34bfaffa46ea2fb1636aa7934bf20a9be2b8e9f5
pare0028 <arthur.parent@etudidant.univ-reims.fr>
2024-02-01 18:29:52 +0100

    Ajout de la ressource api FamilleAnimal

--- src/Entity/FamilleAnimal.php
+++ src/Entity/FamilleAnimal.php
@@ -2,13 +2,14 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiResource;
 use App\Repository\FamilleAnimalRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
-
 #[ORM\Entity(repositoryClass: FamilleAnimalRepository::class)]
+#[ApiResource]
 class FamilleAnimal
 {
     #[ORM\Id]
