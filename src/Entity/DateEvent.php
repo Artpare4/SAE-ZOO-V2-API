91cd683882546c9b39526586800fbea70d7f3218
leco0107 <erwan.lecomte@etudiant.univ-reims.fr>
2024-02-04 18:15:45 +0100

    feat: Création d'un lien entre l'entité DateEvent et Event avec une association séparée

--- src/Entity/DateEvent.php
+++ src/Entity/DateEvent.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\DateEventRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
@@ -17,6 +19,14 @@ class DateEvent
     #[ORM\Column(type: Types::DATE_MUTABLE)]
     private ?\DateTimeInterface $date = null;
 
+    #[ORM\OneToMany(mappedBy: 'dateEvent', targetEntity: AssoEventDateEvent::class)]
+    private Collection $events;
+
+    public function __construct()
+    {
+        $this->events = new ArrayCollection();
+    }
+
     public function getId(): ?int
     {
         return $this->id;
@@ -30,6 +40,36 @@ class DateEvent
     public function setDate(\DateTimeInterface $date): static
     {
         $this->date = $date;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, AssoEventDateEvent>
+     */
+    public function getEvents(): Collection
+    {
+        return $this->events;
+    }
+
+    public function addEvent(AssoEventDateEvent $event): static
+    {
+        if (!$this->events->contains($event)) {
+            $this->events->add($event);
+            $event->setDateEvent($this);
+        }
+
+        return $this;
+    }
+
+    public function removeEvent(AssoEventDateEvent $event): static
+    {
+        if ($this->events->removeElement($event)) {
+            // set the owning side to null (unless already changed)
+            if ($event->getDateEvent() === $this) {
+                $event->setDateEvent(null);
+            }
+        }
 
         return $this;
     }
