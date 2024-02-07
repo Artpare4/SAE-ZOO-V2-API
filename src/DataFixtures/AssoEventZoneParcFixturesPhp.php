d8fd11eb70f08fdf9c9ebe88b37895242ba77e59
Romain Daunat <romain.daunat@etudiant.univ-reims.fr>
2024-02-07 22:33:55 +0100

    feat : Modification des fixtures sur AssoEventZoneParc

--- src/DataFixtures/AssoEventZoneParcFixturesPhp.php
+++ src/DataFixtures/AssoEventZoneParcFixturesPhp.php
@@ -2,6 +2,10 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\AssoEventZoneParc;
+use App\Factory\AssoEventZoneParcFactory;
+use App\Factory\EventFactory;
+use App\Factory\ZoneParcFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,9 +13,24 @@ class AssoEventZoneParcFixturesPhp extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
-
+        $assos = AssoEventZoneParcFactory::createMany(20, function () {
+            return [
+                'event' => EventFactory::random(),
+                'zoneParc' => ZoneParcFactory::random(), ];
+        });
+        foreach ($assos as $asso) {
+            if ($asso instanceof AssoEventZoneParc) {
+                $manager->persist($asso);
+            }
+        }
         $manager->flush();
+    }
+
+    public function getDependencies(): array
+    {
+        return [
+            EventFixtures::class,
+            ZoneParcFixtures::class,
+        ];
     }
 }
