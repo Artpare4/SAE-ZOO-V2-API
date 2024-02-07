6dce9b0c4db32a24895593a0d54600612c1f26e7
Romain Daunat <romain.daunat@etudiant.univ-reims.fr>
2024-02-07 22:56:23 +0100

    feat : Modification des fixtures sur AssoEventAnimal

--- src/DataFixtures/AssoEventAnimalFixtures.php
+++ src/DataFixtures/AssoEventAnimalFixtures.php
@@ -2,6 +2,10 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\AssoEventAnimal;
+use App\Factory\AnimalFactory;
+use App\Factory\AssoEventAnimalFactory;
+use App\Factory\EventFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,9 +13,24 @@ class AssoEventAnimalFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
-
+        $assos = AssoEventAnimalFactory::createMany(20, function () {
+            return [
+                'event' => EventFactory::random(),
+                'animal' => AnimalFactory::random(), ];
+        });
+        foreach ($assos as $asso) {
+            if ($asso instanceof AssoEventAnimal) {
+                $manager->persist($asso);
+            }
+        }
         $manager->flush();
+    }
+
+    public function getDependencies(): array
+    {
+        return [
+            EventFixtures::class,
+            AnimalFixtures::class,
+        ];
     }
 }
