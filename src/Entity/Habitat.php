31c32b92a6e2ff88bdc7978dcf6d135d77623c8f
leco0107 <erwan.lecomte@etudiant.univ-reims.fr>
2024-02-11 16:38:39 +0100

    fix(circular): Supression des Circular References sur Habitat

--- src/Entity/Habitat.php
+++ src/Entity/Habitat.php
@@ -2,12 +2,14 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiResource;
 use App\Repository\HabitatRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: HabitatRepository::class)]
+#[ApiResource]
 class Habitat
 {
     #[ORM\Id]
