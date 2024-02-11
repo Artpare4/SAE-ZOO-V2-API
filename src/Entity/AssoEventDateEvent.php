5b69eac3b7db72e1770acb189fbdb81707d5bc3a
leco0107 <erwan.lecomte@etudiant.univ-reims.fr>
2024-02-11 16:40:06 +0100

    fix(circular): Supression des Circular References sur AssoEventDateEvent

--- src/Entity/AssoEventDateEvent.php
+++ src/Entity/AssoEventDateEvent.php
@@ -2,10 +2,12 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiResource;
 use App\Repository\AssoEventDateEventRepository;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: AssoEventDateEventRepository::class)]
+#[ApiResource]
 class AssoEventDateEvent
 {
     #[ORM\Id]
