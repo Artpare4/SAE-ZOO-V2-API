933008987bcf534d0f18498493151f4f4e631a9a
leco0107 <erwan.lecomte@etudiant.univ-reims.fr>
2024-02-11 16:44:29 +0100

    fix: changement sur l'ortographe de certaines méthodes de Reservation

--- src/Entity/Reservation.php
+++ src/Entity/Reservation.php
@@ -35,7 +35,7 @@ class Reservation
     #[ORM\JoinColumn(nullable: false)]
     private ?User $user = null;
 
-    #[ORM\OneToMany(mappedBy: 'reservations', targetEntity: AssoEventReservation::class)]
+    #[ORM\OneToMany(mappedBy: 'reservation', targetEntity: AssoEventReservation::class)]
     private Collection $events;
 
     public function __construct()
@@ -120,7 +120,7 @@ class Reservation
     {
         if (!$this->events->contains($event)) {
             $this->events->add($event);
-            $event->setReservations($this);
+            $event->setReservation($this);
         }
 
         return $this;
@@ -130,8 +130,8 @@ class Reservation
     {
         if ($this->events->removeElement($event)) {
             // set the owning side to null (unless already changed)
-            if ($event->getReservations() === $this) {
-                $event->setReservations(null);
+            if ($event->getReservation() === $this) {
+                $event->setReservation(null);
             }
         }
 
