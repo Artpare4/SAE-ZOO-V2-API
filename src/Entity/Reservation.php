d05a3bb2923fcb6b402b91b1419804500b9bd002
pare0028 <arthur.parent@etudidant.univ-reims.fr>
2024-02-12 22:07:17 +0100

    Ajout de la liste des opérations pour l'entité Réservation

--- src/Entity/Reservation.php
+++ src/Entity/Reservation.php
@@ -3,6 +3,10 @@
 namespace App\Entity;
 
 use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Delete;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Post;
 use App\Repository\ReservationRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
@@ -10,7 +14,13 @@ use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: ReservationRepository::class)]
-#[ApiResource]
+#[ApiResource(operations: [
+    new Get(),
+    new GetCollection(),
+    new Post(),
+    new Delete(),
+]
+)]
 class Reservation
 {
     #[ORM\Id]
