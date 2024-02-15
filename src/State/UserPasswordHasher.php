8b1c0bad4c6866cea37de0615c892505ce557dfe
leco0107 <erwan.lecomte@etudiant.univ-reims.fr>
2024-02-15 16:07:09 +0100

    doc(User): Documentation du processeur d'hachage UserPasswordHasher

--- src/State/UserPasswordHasher.php
+++ src/State/UserPasswordHasher.php
@@ -8,6 +8,11 @@ use App\Entity\User;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 /**
+ * Processeur de User permettant le hachage du mot de passe après entré en API.
+ *
+ * Ecriture d'un processeur hachant automatiquement le mot de passe d'un utilisateur avant de le mettre en base de données.
+ * Principe de processeurs trouvé directement dans la doc d'API Platform à l'adresse :
+ * https://api-platform.com/docs/core/user/
  * @implements ProcessorInterface<User, User|void>
  */
 final class UserPasswordHasher implements ProcessorInterface
@@ -19,6 +24,10 @@ final class UserPasswordHasher implements ProcessorInterface
     }
 
     /**
+     * Méthode de process
+     * Récupère le plain-password de l'utilisateur, le hache et le stocke dans le password.
+     * Après attribution, le plain-password est supprimé afin, d'éviter une faille de sécurité.
+     * Ajoute/Modifie ensuite l'utilisateur dans la base.
      * @param User $data
      */
     public function process(mixed $data, Operation $operation, array $uriVariables = [], array $context = []): User|null
