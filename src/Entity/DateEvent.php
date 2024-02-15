cf7e41dbbb0f949035bf3a5e46bf2c6b90518a64
Romain Daunat <romain.daunat@etudiant.univ-reims.fr>
2024-02-15 17:37:18 +0100

    feat(DateEvent) : ajout du groupe de normalisation Event_read

--- src/Entity/DateEvent.php
+++ src/Entity/DateEvent.php
@@ -8,6 +8,7 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 #[ORM\Entity(repositoryClass: DateEventRepository::class)]
 #[ApiResource]
@@ -16,9 +17,11 @@ class DateEvent
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    #[Groups(['Event_read'])]
     private ?int $id = null;
 
     #[ORM\Column(type: Types::DATE_MUTABLE)]
+    #[Groups(['Event_read'])]
     private ?\DateTimeInterface $dateEvent = null;
 
     #[ORM\OneToMany(mappedBy: 'dateEvent', targetEntity: AssoEventDateEvent::class)]
