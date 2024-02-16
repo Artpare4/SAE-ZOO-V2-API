f7cecda47a17b0a7fb351c37fda06d5847d4c5ad
Romain MONNEY <Romain.monney@etudiant.univ-reims.fr>
2024-02-16 18:48:14 +0100

    ajout api ZoneParc get by id

--- src/Entity/ZoneParc.php
+++ src/Entity/ZoneParc.php
@@ -3,13 +3,19 @@
 namespace App\Entity;
 
 use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
 use App\Repository\ZoneParcRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: ZoneParcRepository::class)]
-#[ApiResource]
+#[ApiResource(operations: [
+    new Get(openapiContext: [
+        'summary' => 'Retourne les informations de la zone parc associée à l\'id',
+        'description' => 'Retourne les informations de la zone parc associée à l\'id',
+    ])])]
 class ZoneParc
 {
     #[ORM\Id]
