72593bbb78e9832dfbf764d15b4c3f19031eb4c8
pare0028 <arthur.parent@etudidant.univ-reims.fr>
2024-02-18 12:27:07 +0100

    Modification de la forge de donnée de l'entité Event pour prendre en compte les images

--- src/Factory/EventFactory.php
+++ src/Factory/EventFactory.php
@@ -11,21 +11,21 @@ use Zenstruck\Foundry\RepositoryProxy;
 /**
  * @extends ModelFactory<Event>
  *
- * @method        Event|Proxy create(array|callable $attributes = [])
- * @method static Event|Proxy createOne(array $attributes = [])
- * @method static Event|Proxy find(object|array|mixed $criteria)
- * @method static Event|Proxy findOrCreate(array $attributes)
- * @method static Event|Proxy first(string $sortedField = 'id')
- * @method static Event|Proxy last(string $sortedField = 'id')
- * @method static Event|Proxy random(array $attributes = [])
- * @method static Event|Proxy randomOrCreate(array $attributes = [])
+ * @method        Event|Proxy                     create(array|callable $attributes = [])
+ * @method static Event|Proxy                     createOne(array $attributes = [])
+ * @method static Event|Proxy                     find(object|array|mixed $criteria)
+ * @method static Event|Proxy                     findOrCreate(array $attributes)
+ * @method static Event|Proxy                     first(string $sortedField = 'id')
+ * @method static Event|Proxy                     last(string $sortedField = 'id')
+ * @method static Event|Proxy                     random(array $attributes = [])
+ * @method static Event|Proxy                     randomOrCreate(array $attributes = [])
  * @method static EventRepository|RepositoryProxy repository()
- * @method static Event[]|Proxy[] all()
- * @method static Event[]|Proxy[] createMany(int $number, array|callable $attributes = [])
- * @method static Event[]|Proxy[] createSequence(iterable|callable $sequence)
- * @method static Event[]|Proxy[] findBy(array $attributes)
- * @method static Event[]|Proxy[] randomRange(int $min, int $max, array $attributes = [])
- * @method static Event[]|Proxy[] randomSet(int $number, array $attributes = [])
+ * @method static Event[]|Proxy[]                 all()
+ * @method static Event[]|Proxy[]                 createMany(int $number, array|callable $attributes = [])
+ * @method static Event[]|Proxy[]                 createSequence(iterable|callable $sequence)
+ * @method static Event[]|Proxy[]                 findBy(array $attributes)
+ * @method static Event[]|Proxy[]                 randomRange(int $min, int $max, array $attributes = [])
+ * @method static Event[]|Proxy[]                 randomSet(int $number, array $attributes = [])
  */
 final class EventFactory extends ModelFactory
 {
@@ -43,11 +43,19 @@ final class EventFactory extends ModelFactory
     protected function getDefaults(): array
     {
         $liste = ['Spectacle avec les oiseaux', 'Ballet de dauphins', 'Activités ludiques pour enfants', 'Visites guidées', 'Expérience de soigneur de pingouin'];
+        $path = 'public/image/events/*';
+        $files = glob($path);
+        $filesAndPath = [];
+        foreach ($files as $file) {
+            $filesAndPath[] = $path.$file;
+        }
+        $choiceFile = array_rand($filesAndPath);
 
         return [
             'description' => self::faker()->text(512),
             'nbPlaces' => self::faker()->numberBetween(50, 100),
             'nomEvent' => self::faker()->randomElement($liste),
+            'imgEvent' => $choiceFile,
         ];
     }
 
@@ -58,7 +66,7 @@ final class EventFactory extends ModelFactory
     {
         return $this
             // ->afterInstantiate(function(Event $event): void {})
-            ;
+        ;
     }
 
     protected static function getClass(): string
