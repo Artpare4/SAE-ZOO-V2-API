7df57839a35786cd3bc0a00f547ffd7881d806ed
pare0028 <arthur.parent@etudidant.univ-reims.fr>
2024-02-27 21:10:38 +0100

    Ajout du test qui permet de récupérer une liste d'event filtré sur leur noms

--- tests/Api/Event/EventGetCollectionDataValisationCest.php
+++ tests/Api/Event/EventGetCollectionDataValisationCest.php
@@ -36,4 +36,35 @@ class EventGetCollectionDataValisationCest
         $apiTester->seeResponseContainsJson(['hydra:totalItems' => 4]);
         $apiTester->seeResponseMatchesJsonType(self::expectedProperties(), '$["hydra:member"][0]');
     }
+
+    public function getFilteredListEvent(ApiTester $apiTester)
+    {
+        EventFactory::createSequence([
+            [
+            'nomEvent' => 'Event1Test',
+            ],
+            [
+                'nomEvent' => 'Event2',
+            ],
+            [
+                'nomEvent' => 'TestEvent3',
+            ],
+            [
+                'nomEvent' => 'Event4',
+            ],
+            ]);
+
+        $apiTester->sendGet('/api/events?nomEvent=Test');
+
+        $apiTester->seeResponseCodeIsSuccessful();
+        $apiTester->seeResponseIsJson();
+        $apiTester->seeResponseIsACollection(Event::class, '/api/events', [
+            'hydra:member' => 'array',
+            'hydra:totalItems' => 'integer',
+            'hydra:search' => 'array',
+        ]);
+        $apiTester->seeResponseContainsJson(['hydra:totalItems' => 2]);
+        $apiTester->seeResponseMatchesJsonType(self::expectedProperties(), '$["hydra:member"][0]');
+        $apiTester->seeResponseMatchesJsonType(self::expectedProperties(), '$["hydra:member"][1]');
+    }
 }
