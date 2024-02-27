70212f59c20e15d4d22cb388e6a8e4bc89234c15
pare0028 <arthur.parent@etudidant.univ-reims.fr>
2024-02-27 23:08:01 +0100

    Ajout du test vérifiant l'accès aux donnée d'un animal mort

--- tests/Api/Animal/AnimalGetCollectionDataValisationCest.php
+++ tests/Api/Animal/AnimalGetCollectionDataValisationCest.php
@@ -23,6 +23,22 @@ class AnimalGetCollectionDataValisationCest
         ];
     }
 
+    protected static function expectedPropertiesForDeadAnimal(): array
+    {
+        return [
+            'id' => 'integer',
+            'nomAnimal' => 'string',
+            'dateNaissance' => 'string:date',
+            'dateMort' => 'string:date',
+            'taille' => 'integer|float',
+            'poids' => 'integer|float',
+            'familleAnimal' => 'array|string',
+            'caracteristique' => 'string',
+            'events' => 'array',
+            'imgAnimal' => 'string',
+        ];
+    }
+
     public function getLivingAnimalDetails(ApiTester $apiTester)
     {
         $famille = FamilleAnimalFactory::createOne()->object();
@@ -38,5 +54,24 @@ class AnimalGetCollectionDataValisationCest
         $apiTester->seeResponseCodeIsSuccessful();
         $apiTester->seeResponseIsJson();
         $apiTester->seeResponseIsAnItem(self::expectedPropertiesForLivingAnimal());
+    }
+
+    public function getDeadAnimalDetails(ApiTester $apiTester)
+    {
+        $famille = FamilleAnimalFactory::createOne()->object();
+
+        $data = [
+            'dateNaissance' => new \DateTime('2010-10-04 18:39:16'),
+            'dateMort' => new \DateTime('2023-12-12 20:43:56'),
+            'familleAnimal' => $famille,
+            'taille' => 1,
+            'poids' => 1.0,
+        ];
+        AnimalFactory::createOne($data);
+
+        $apiTester->sendGet('/api/animals/1');
+        $apiTester->seeResponseCodeIsSuccessful();
+        $apiTester->seeResponseIsJson();
+        $apiTester->seeResponseIsAnItem(self::expectedPropertiesForDeadAnimal());
     }
 }
