0eeb5989043818b392b599d479783270501f286c
pare0028 <arthur.parent@etudidant.univ-reims.fr>
2024-02-27 17:08:20 +0100

    Ajout du test d'accès de l'image d'une zone du parc

--- tests/Api/ZoneParc/ZoneParcGetDataValisationCest.php
+++ tests/Api/ZoneParc/ZoneParcGetDataValisationCest.php
@@ -31,4 +31,16 @@ class ZoneParcGetDataValisationCest
         $apiTester->seeResponseIsJson();
         $apiTester->seeResponseIsAnItem(self::expectedProperties(), $data);
     }
+
+    public function getImageZoneParc(ApiTester $apiTester)
+    {
+        $data = [
+            'imgZone' => 'public/image/zone_parc/zone_illustration.jpg',
+        ];
+        $zone = ZoneParcFactory::createOne($data)->object();
+        $apiTester->sendGet('/api/zone_parcs/1/image');
+        $apiTester->seeResponseCodeIsSuccessful();
+        $apiTester->seeHttpHeader('Content-Type', 'image/jpeg');
+        $apiTester->seeResponseContains(file_get_contents($zone->getImgZone(), true));
+    }
 }
