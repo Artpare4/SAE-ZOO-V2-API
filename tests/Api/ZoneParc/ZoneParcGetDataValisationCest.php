7f19d76540e8e8c58245583e5e96e1e1257a30a3
pare0028 <arthur.parent@etudidant.univ-reims.fr>
2024-02-27 15:49:06 +0100

    Ajout de test getZoneParcDetail qui vérifie l'accès à une zone du parc

--- tests/Api/ZoneParc/ZoneParcGetDataValisationCest.php
+++ tests/Api/ZoneParc/ZoneParcGetDataValisationCest.php
@@ -1,10 +1,34 @@
 <?php
 
-
 namespace App\Tests\Api\ZoneParc;
 
+use App\Factory\ZoneParcFactory;
 use App\Tests\Support\ApiTester;
 
 class ZoneParcGetDataValisationCest
 {
+    protected static function expectedProperties(): array
+    {
+        return [
+            'id' => 'integer',
+            'libZone' => 'string',
+            'familleAnimals' => 'array',
+            'events' => 'array',
+            'imgZone' => 'string',
+        ];
+    }
+
+    public function getZoneParcDetail(ApiTester $apiTester)
+    {
+        $data = [
+            'libZone' => 'Zone1',
+            'imgZone' => 'ImageZoneParc',
+        ];
+        ZoneParcFactory::createOne($data);
+
+        $apiTester->sendGet('/api/zone_parcs/1');
+        $apiTester->seeResponseCodeIsSuccessful();
+        $apiTester->seeResponseIsJson();
+        $apiTester->seeResponseIsAnItem(self::expectedProperties(), $data);
+    }
 }
