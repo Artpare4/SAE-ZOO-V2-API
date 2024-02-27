1f6ca42c34e5d6b31e945528417966a52f65aadb
pare0028 <arthur.parent@etudidant.univ-reims.fr>
2024-02-27 21:36:03 +0100

    Ajout du test vérifiant l'accès à une ressouce de l'entité FamilleAnimal avec l'api

---
+++ tests/Api/FamilleAnimal/FamilleAnimalGetDataValisationCest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Tests\Api\FamilleAnimal;
+
+use App\Factory\EspeceFactory;
+use App\Factory\FamilleAnimalFactory;
+use App\Factory\ZoneParcFactory;
+use App\Tests\Support\ApiTester;
+
+class FamilleAnimalGetDataValisationCest
+{
+    protected function expectedProperties(): array
+    {
+        return [
+            'id' => 'integer',
+            'nomFamilleAnimal' => 'string',
+            'nomScientifique' => 'string',
+            'dangerExtinction' => 'integer',
+            'description' => 'string',
+            'typeAlimentation' => 'string',
+            'zoneParc' => 'array',
+            'animals' => 'array',
+            'espece' => 'array',
+            'assoHabitatFamilleAnimals' => 'array',
+        ];
+    }
+
+    public function getFamilleAnimalDetail(ApiTester $apiTester)
+    {
+        $zone = ZoneParcFactory::createOne()->object();
+        $espece = EspeceFactory::createOne()->object();
+        $data = [
+            'zoneParc' => $zone,
+            'espece' => $espece,
+        ];
+        FamilleAnimalFactory::createOne($data);
+
+        $apiTester->sendGet('/api/famille_animals/1');
+
+        $apiTester->seeResponseCodeIsSuccessful();
+        $apiTester->seeResponseIsJson();
+        $apiTester->seeResponseIsAnItem(self::expectedProperties());
+    }
+}
