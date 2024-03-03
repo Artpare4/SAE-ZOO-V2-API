adf334db64136b153269338771e2a20d60970d50
leco0107 <erwan.lecomte@etudiant.univ-reims.fr>
2024-03-03 14:09:20 +0100

    test(User): Ajout des tests lié au GetMe d'utilisateur

---
+++ tests/Api/User/UserGetMeCest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Tests\Api\User;
+
+use App\Entity\User;
+use App\Factory\UserFactory;
+use App\Tests\Support\ApiTester;
+
+class UserGetMeCest
+{
+    protected static function expectedProperties(): array
+    {
+        return [
+            'id' => 'integer',
+            'email' => 'string',
+            'reservations' => 'array',
+            'phone' => 'string',
+            'nomUser' => 'string',
+            'pnomUser' => 'string',
+        ];
+    }
+
+    public function anonymousMeIsUnauthorized(ApiTester $I): void
+    {
+        // 1. Arrange
+        UserFactory::createOne();
+
+        // 2. Act
+        $I->sendGet('/api/me');
+
+        // 3. Assert
+        $I->seeResponseCodeIs(401);
+    }
+
+    /*
+    public function authenticatedUserCanGetMe(ApiTester $I): void
+    {
+        // 1. Arrange
+        $user = UserFactory::createOne()->object();
+        $I->amLoggedInAs($user);
+        UserFactory::createOne();
+
+        // 2. Act
+        $I->sendGet('/api/me');
+
+        // 3. Assert
+        $I->seeResponseCodeIsSuccessful();
+        $I->seeResponseIsJson();
+        $I->seeResponseIsAnEntity(User::class, '/api/me');
+        $I->seeResponseIsAnItem(self::expectedProperties());
+
+    }
+    */
+}
