265e30fe9bbfb7b7416f8740a6e9b0cd9e027490
pare0028 <arthur.parent@etudiant.univ-reims.fr>
2024-04-01 13:00:29 +0200

    Ajout des nouveaux champs du formulaires d'inscription pour avoir plus d'information concernant l'utilisateur

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -4,11 +4,12 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
 
@@ -17,28 +18,43 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('agreeTerms', CheckboxType::class, [
-                                'mapped' => false,
+            ->add('nomUser', TextType::class, ['empty_data' => '',
                 'constraints' => [
-                    new IsTrue([
-                        'message' => 'You should agree to our terms.',
-                    ]),
-                ],
+                    new Length([
+                            'max' => 128,
+                            'maxMessage' => 'Votre nom doit faire au maximum {{ limit }} caractères']
+                    ), ]])
+            ->add('pnomUser', TextType::class, ['empty_data' => '',
+                'constraints' => [
+                    new Length([
+                            'max' => 128,
+                            'maxMessage' => 'Votre prénom doit faire au maximum {{ limit }} caractères']
+                    ), ]])
+            ->add('phoneUser', TelType::class, ['empty_data' => '',
+                'constraints' => [
+                    new Length([
+                            'min' => 10,
+                            'minMessage' => 'Votre numéro de téléphone est trop court, il doit faire au minimum {{ limit }} caractères',
+                            'max' => 10,
+                            'maxMessage' => 'Votre numéro de téléphone doit faire au maximum {{ limit }} caractères']
+                    ), ]])
+            ->add('email', EmailType::class, ['empty_data' => '',
+                'constraints' => [
+                    new Length([
+                            'max' => 180,
+                            'maxMessage' => 'Votre email doit faire au maximum {{ limit }} caractères']
+                    ), ],
             ])
             ->add('plainPassword', PasswordType::class, [
-                                // instead of being set onto the object directly,
-                // this is read and encoded in the controller
                 'mapped' => false,
                 'attr' => ['autocomplete' => 'new-password'],
                 'constraints' => [
                     new NotBlank([
-                        'message' => 'Please enter a password',
+                        'message' => 'Entrez un mot de passe',
                     ]),
                     new Length([
                         'min' => 6,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
-                        // max length allowed by Symfony for security reasons
+                        'minMessage' => 'Votre mot de passe doit faire au minimum {{ limit }} caractères',
                         'max' => 4096,
                     ]),
                 ],
