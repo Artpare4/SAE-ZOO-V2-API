4c78499f58797448553932165ae8bfa54ae72d04
pare0028 <arthur.parent@etudiant.univ-reims.fr>
2024-04-04 14:11:20 +0200

    Ajout du contrôleur User contenant seulement la route pour le formulaire de modification des données utilisateur

---
+++ src/Controller/UserController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Controller;
+
+use App\Form\RegistrationFormType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UserController extends AbstractController
+{
+    #[Route('/user/update', name: 'app_user_update')]
+    public function update(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        if (!$this->isGranted('ROLE_USER')) {
+            return $this->redirectToRoute('app_login');
+        }
+        $user = $this->getUser();
+        $form = $this->createForm(RegistrationFormType::class, $user);
+        $form->remove('plainPassword');
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user = $form->getData();
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_user');
+        }
+        return $this->render('user/update.html.twig', [
+            'controller_name' => 'UserController',
+            'form' => $form,
+        ]);
+    }
+}
